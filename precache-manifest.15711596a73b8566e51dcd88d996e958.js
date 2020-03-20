fd4f2fef9d9b9ffdf478e3322ad4e77648f908ea
TheRealMentor <sharadr@iitbhilai.ac.in>
2020-03-20 23:04:51 +0530

    Updates

---
+++ precache-manifest.15711596a73b8566e51dcd88d996e958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a3569d58c75f8b4548309e0b705d09",
+    "url": "/crud-app/index.html"
+  },
+  {
+    "revision": "62b2639471d041e8227e",
+    "url": "/crud-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "225bcfd7c66c9c00844e",
+    "url": "/crud-app/static/css/main.e38b3029.chunk.css"
+  },
+  {
+    "revision": "62b2639471d041e8227e",
+    "url": "/crud-app/static/js/2.afd41364.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/crud-app/static/js/2.afd41364.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "225bcfd7c66c9c00844e",
+    "url": "/crud-app/static/js/main.48132152.chunk.js"
+  },
+  {
+    "revision": "31a2e40e4c4ce08b01a0",
+    "url": "/crud-app/static/js/runtime-main.8592236d.js"
+  }
+]);
